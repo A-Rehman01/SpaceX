77ae6d8857f2378838ccfe210b884ec93d686e2a
abdulrehman <arehmana377@gmail.com>
2020-09-03 21:57:25 +0500

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 //Components
-import { Index } from '../Launch/index'
+// import { Index } from '../Launch/index'
 import { IndexDetail } from '../LaunchDetail/index'
 // import { Launch } from '../Launch/Launch';
 function App() {
