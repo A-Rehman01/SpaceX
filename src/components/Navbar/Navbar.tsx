2c8beae620bb28c05d967a39e0c27a4b9d47fc71
abdulrehman <arehmana377@gmail.com>
2020-09-04 02:06:44 +0500

    Complete

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import '../App/App.css'
+import logo from  '../assests/spacex.svg'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
         <div className='Navbar'>
-            
+            <div className='logodiv'>
+            <Link className='linkss' to='/'>
+            <img className='logo' src={logo} alt=""/>
+            </Link>
+            </div> 
+           <div className='gitdiv'>
+            <a  className='linkss' href="https://github.com/A-Rehman01/SpaceX" rel="noopener noreferrer" target='_blank'>   
+            <GitHubIcon className='git'/>
+            </a>
+            </div> 
         </div>
     )
 }
